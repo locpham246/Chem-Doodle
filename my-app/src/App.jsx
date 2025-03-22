27ed21bade5af8051fb6e87afe98803013aff404
eliobaby <157168792+eliobaby@users.noreply.github.com>
2025-03-22 18:22:54 -0500

    Smiles parsing

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,13 +1,9 @@
-// src/App.jsx
 import React from 'react';
 import ChemDrawer from './ChemDrawer';
-import './index.css';
 
 function App() {
   return (
-    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
-      <h1>Chemical Drawer Demo</h1>
-      <p>Draw your chemical structure below:</p>
+    <div>
       <ChemDrawer />
     </div>
   );
