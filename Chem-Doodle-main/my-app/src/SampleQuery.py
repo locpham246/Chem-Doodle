ffb9200d46020b56dabb7a3cc294386e3c55e96c
JordanCarden <154265139+JordanCarden@users.noreply.github.com>
2025-04-28 15:10:56 -0500

    Update SampleQuery.py

--- Chem-Doodle-main/my-app/src/SampleQuery.py
+++ Chem-Doodle-main/my-app/src/SampleQuery.py
@@ -1,8 +1,8 @@
 from cassandra.cluster import Cluster
 
 cluster = Cluster(['96.125.118.113'], port=9042)
-session = cluster.connect('your_keyspace')
+session = cluster.connect('drug_discovery')
 
-rows = session.execute('SELECT * FROM your_table LIMIT 10')
+rows = session.execute('SELECT * FROM compounds LIMIT 10')
 for row in rows:
     print(row)
